b1340348c8b9abb549b52279a480bde7212e5e89
Hannah Witvrouwen <H.Witvrouwen@chipsoft.com>
2025-06-23 15:54:37 +0200

    enable friendly lua snips and use official copilot plugin

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,19 +1,8 @@
 return {
-  'zbirenbaum/copilot.lua',
-  event = 'InsertEnter',
-  config = function()
-    require('copilot').setup {
-      panel = {
-        auto_refresh = true,
-        enabled = true,
-      },
-      suggestions = {
-        auto_trigger = true,
-        enabled = true,
-      },
-      filetypes = {
-        ['.'] = true,
-      },
-    }
-  end,
+  'github/copilot.vim',
+  -- event = 'InsertEnter',
+  -- config = function()
+  --   require('copilot').setup {
+  --   }
+  -- end,
 }
