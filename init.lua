ef6d358d616b8b3f50f1e276efc9ea8996a6c825
xunafay <me@xunafay.io>
2025-06-28 21:00:42 +0200

    underline lsp warnings too

--- init.lua
+++ init.lua
@@ -642,7 +642,7 @@ require('lazy').setup({
       vim.diagnostic.config {
         severity_sort = true,
         float = { border = 'rounded', source = 'if_many' },
-        underline = { severity = vim.diagnostic.severity.ERROR },
+        underline = { severity = vim.diagnostic.severity.WARN },
         signs = vim.g.have_nerd_font and {
           text = {
             [vim.diagnostic.severity.ERROR] = '󰅚 ',
