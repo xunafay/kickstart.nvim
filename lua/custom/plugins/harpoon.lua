e75672d7dcbc92ede14d1561fbd988e28f871731
xunafay <me@xunafay.io>
2025-06-29 22:28:06 +0200

    add harpoon config

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -4,4 +4,63 @@ return {
     'nvim-lua/plenary.nvim',
   },
   branch = 'harpoon2',
+  setup = function()
+    local harpoon = require 'harpoon'
+
+    -- REQUIRED
+    harpoon:setup()
+    -- REQUIRED
+
+    local harpoon_extensions = require 'harpoon.extensions'
+    harpoon:extend(harpoon_extensions.builtins.highlight_current_file())
+
+    -- basic telescope configuration
+    local conf = require('telescope.config').values
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
+
+      require('telescope.pickers')
+        .new({}, {
+          prompt_title = 'Harpoon',
+          finder = require('telescope.finders').new_table {
+            results = file_paths,
+          },
+          previewer = conf.file_previewer {},
+          sorter = conf.generic_sorter {},
+        })
+        :find()
+    end
+
+    vim.keymap.set('n', '<leader>ha', function()
+      harpoon:list():add()
+    end)
+
+    vim.keymap.set('n', '<leaderhh>', function()
+      toggle_telescope(harpoon:list())
+    end, { desc = 'Open harpoon window' })
+
+    vim.keymap.set('n', '<leader>hn', function()
+      harpoon:list():select(1)
+    end)
+    vim.keymap.set('n', '<leader>he', function()
+      harpoon:list():select(2)
+    end)
+    vim.keymap.set('n', '<leader>ho', function()
+      harpoon:list():select(3)
+    end)
+    vim.keymap.set('n', '<leader>hi', function()
+      harpoon:list():select(4)
+    end)
+
+    -- Toggle previous & next buffers stored within Harpoon list
+    vim.keymap.set('n', '<leader>hp', function()
+      harpoon:list():prev()
+    end)
+    vim.keymap.set('n', '<leader>hf', function()
+      harpoon:list():next()
+    end)
+  end,
 }
