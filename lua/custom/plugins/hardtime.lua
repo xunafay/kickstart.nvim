15c215354c0fb5c2f544762bddd3bbfefe12837b
Hannah Witvrouwen <hannah.witvrouwen@gmail.com>
2026-02-23 22:43:18 +0100

    add dadbot and hardtime

---
+++ lua/custom/plugins/hardtime.lua
@@ -0,0 +1,19 @@
+return {
+  'm4xshen/hardtime.nvim',
+  lazy = false,
+  dependencies = { 'MunifTanjim/nui.nvim' },
+  opts = {
+    restricted_keys = {
+      ['<Up>'] = { 'n', 'x' },
+      ['<Down>'] = { 'n', 'x' },
+      ['<Left>'] = { 'n', 'x' },
+      ['<Right>'] = { 'n', 'x' },
+    },
+    disabled_keys = {
+      ['<Up>'] = {},
+      ['<Down>'] = {},
+      ['<Left>'] = {},
+      ['<Right>'] = {},
+    },
+  },
+}
